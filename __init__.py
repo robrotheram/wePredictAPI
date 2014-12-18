c680f6f9e4a14db27c68d5db5849bc5a54314d8f
robert <robrotheram@gmail.com>
2014-12-18 17:28:49 +0000

    update to init message

--- __init__.py
+++ __init__.py
@@ -22,43 +22,20 @@ def getAdress():
         return jsonify(data = cursor.fetchall())
 
 @app.route('/getsmoking')
-def getSmoking():
+def getsmoking():
     limit = request.args.get('limit')
-
     if limit is None:
         db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
         cursor = db.cursor()
-        cursor.execute("SELECT * from SMOKING");
+        cursor.execute("SELECT * from ADRESS");
         db.close()
         return jsonify(data = cursor.fetchall())
     else:
         db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
         cursor = db.cursor()
-        cursor.execute("SELECT * from SMOKING LIMIT "+limit);
+        cursor.execute("SELECT * from ADRESS LIMIT "+limit);
         db.close()
         return jsonify(data = cursor.fetchall())
-
-@app.route('/getpolution')
-def getPolution():
-    limit = request.args.get('limit')
-    return "POPULSTION"
-
-
-@app.route('/getflu')
-def getFlu():
-    limit = request.args.get('limit')
-    return "FLU"
-
-@app.route('/getcopd')
-def getCopd():
-    limit = request.args.get('limit')
-    return "COPD"
-
-
-@app.route('/getasthamqof(')
-def getAsthamQof():
-    limit = request.args.get('limit')
-    return "ASMATHA"
 
 
 
