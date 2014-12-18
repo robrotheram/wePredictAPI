6d0fe6f3ce6dbcf58151767dd3d76587769afe13
robertfletcher <robrotheram@gmail.com>
2014-12-18 23:41:51 +0000

    Added test

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
+
 import MySQLdb
 from decimal import *
 
@@ -88,17 +90,18 @@ def getAsmtha():
         cursor.execute("SELECT * from ASTHMA_QOF LIMIT "+limit);
         db.close()
         return jsonify(data = cursor.fetchall())
+
 @app.route('/getpollution')
 def getpollution():
     limit = request.args.get('limit')
     if limit is None:
-        db = MySQLdb.connect(host="ASTHMA_QOF", port=3306, user="root", passwd="mallard", db="wePredict")
+        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
         cursor = db.cursor()
         cursor.execute("SELECT * from POLLUTION");
         db.close()
         return jsonify(data = cursor.fetchall())
     else:
-        db = MySQLdb.connect(host="ASTHMA_QOF", port=3306, user="root", passwd="mallard", db="wePredict")
+        db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
         cursor = db.cursor()
         cursor.execute("SELECT * from POLLUTION LIMIT "+limit);
         db.close()
@@ -109,6 +112,15 @@ def getpollution():
 def test():
     data = Decimal(19.123)
     return jsonify(data)
+
+@app.route('/test_table')
+def test():
+    db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
+    cursor = db.cursor()
+    cursor.execute("SELECT * from test_table");
+    db.close()
+    return jsonify(data = cursor.fetchall())
+
 
 
 @app.route("/")
