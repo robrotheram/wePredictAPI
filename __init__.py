6699fc2f20c2dce96f651586934826b7420813c9
robertfletcher <robrotheram@gmail.com>
2014-12-19 00:45:59 +0000

    updated test

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from flask import request
 import json
 from flask import Response
 import decimal
+import simplejson
 from flask.ext.sqlalchemy import SQLAlchemy
 
 import MySQLdb
@@ -12,18 +13,6 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 app.config['PROPAGATE_EXCEPTIONS'] = True
-
-
-class DecimalEncoder(json.JSONEncoder):
-    def _iterencode(self, o, markers=None):
-        if isinstance(o, decimal.Decimal):
-            # wanted a simple yield str(o) in the next line,
-            # but that would mean a yield on the line with super(...),
-            # which wouldn't work (see my comment below), so...
-            return (str(o) for o in [o])
-        return super(DecimalEncoder, self)._iterencode(o, markers)
-
-
 
 
 
@@ -139,7 +128,7 @@ def testtable():
     #dataone = data[0]
     filename = "bob"
     js = { "name" : filename}
-    return Response(json.dumps(data,cls=DecimalEncoder),  mimetype='application/json')
+    return Response(simplejson.dumps(data),  mimetype='application/json')
 
 
 
