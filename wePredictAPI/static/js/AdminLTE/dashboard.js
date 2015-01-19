9be76fddd9e6add40cf4b21c4e12110829f7652a
robert <robrotheram@gmail.com>
2015-01-19 18:58:23 +0000

    fixed bugs

--- wePredictAPI/static/js/AdminLTE/dashboard.js
+++ wePredictAPI/static/js/AdminLTE/dashboard.js
@@ -8,25 +8,6 @@
 $(function () {
     "use strict";
 
-    //Make the dashboard widgets sortable Using jquery UI
-    $(".connectedSortable").sortable({
-        placeholder: "sort-highlight",
-        connectWith: ".connectedSortable",
-        handle: ".box-header, .nav-tabs",
-        forcePlaceholderSize: true,
-        zIndex: 999999
-    }).disableSelection();
-    $(".connectedSortable .box-header, .connectedSortable .nav-tabs-custom").css("cursor", "move");
-    //jQuery UI sortable for the todo list
-    $(".todo-list").sortable({
-        placeholder: "sort-highlight",
-        handle: ".handle",
-        forcePlaceholderSize: true,
-        zIndex: 999999
-    }).disableSelection();
-    ;
-
-
     $('.daterange').daterangepicker(
         {
             ranges: {
