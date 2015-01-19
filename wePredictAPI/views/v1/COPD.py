bae6c7fb3a9592e39dae8b627c35df08bbbafd34
robert <robrotheram@gmail.com>
2015-01-19 19:11:49 +0000

    Added  average copd

--- wePredictAPI/views/v1/COPD.py
+++ wePredictAPI/views/v1/COPD.py
@@ -1,6 +1,8 @@
 __author__ = 'robert'
 from flask import jsonify
 from flask import request
+from flask import Response
+import simplejson
 
 from wePredictAPI import app
 import MySQLdb
@@ -21,3 +23,17 @@ def getcopd():
         cursor.execute("SELECT * from COPD LIMIT " + limit);
         db.close()
         return jsonify(data=cursor.fetchall())
+
+
+@app.route('/v1/getcopdaverage')
+def getcopd():
+    db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
+    cursor = db.cursor()
+    cursor.execute("SELECT Avg(Value_09), Avg(Value_10), Avg(Value_11), Avg(Value_12), Avg(Value_13) FROM COPD");
+    db.close()
+    data = cursor.fetchall()
+    js_data = []
+    for obj in data:
+        objjst = {"2009": obj[0], "2010": obj[1], "2011": obj[2], "2012": obj[3], "2013": obj}
+    js_data.append(objjst)
+    return Response(simplejson.dumps(js_data), mimetype='application/json')
