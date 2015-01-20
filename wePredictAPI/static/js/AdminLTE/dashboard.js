41650e5403b3f320435273b521b66fb6920b8111
robert <robrotheram@gmail.com>
2015-01-20 18:27:08 +0000

    bug fix

--- wePredictAPI/static/js/AdminLTE/dashboard.js
+++ wePredictAPI/static/js/AdminLTE/dashboard.js
@@ -139,19 +139,6 @@ $(function () {
         });
     });
 
-
-    //Donut Chart
-    var donut = new Morris.Donut({
-        element: 'sales-chart',
-        resize: true,
-        colors: ["#3c8dbc", "#f56954", "#00a65a"],
-        data: [
-            {label: "Download Sales", value: 12},
-            {label: "In-Store Sales", value: 30},
-            {label: "Mail-Order Sales", value: 20}
-        ],
-        hideHover: 'auto'
-    });
     /*Bar chart
      var bar = new Morris.Bar({
      element: 'bar-chart',
