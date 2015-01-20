9a47978ab697b9e92ce8825c70009a946a624942
robert <robrotheram@gmail.com>
2015-01-20 15:05:39 +0000

    Change format getFlu_2012_adress

--- wePredictAPI/views/v1/flu.py
+++ wePredictAPI/views/v1/flu.py
@@ -1,11 +1,13 @@
 from flask import jsonify
 from flask import request
+from flask import Response
+import simplejson
 
 from wePredictAPI import app
 import MySQLdb
 
 
-@app.route('/getflu')
+@app.route('/v1/getflu')
 def getflu():
     limit = request.args.get('limit')
     if limit is None:
@@ -22,13 +24,19 @@ def getflu():
         return jsonify(data=cursor.fetchall())
 
 
-@app.route('/getflu_2012_address')
+@app.route('/v1/getflu_2012_address')
 def getflu12adress():
     db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="mallard", db="wePredict")
     cursor = db.cursor()
     cursor.execute(
         "SELECT SUM(Value_12), SUBSTRING(Postcode,1,2) As PC FROM FLU Join ADRESS on FLU.Practice_Code = ADRESS.PracticeCode GROUP BY PC;");
     db.close()
-    return jsonify(data=cursor.fetchall())
+    data = cursor.fetchall()
+    js_data = []
+    for obj in data:
+        objjst = {obj[0]: obj[1]}
+        js_data.append(objjst)
+
+    return Response(simplejson.dumps(js_data), mimetype='application/json')
 
 
