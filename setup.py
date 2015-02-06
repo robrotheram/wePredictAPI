47c04720a5985ecbf3714eabe469352d8e3c5b97
robert <robrotheram@gmail.com>
2015-02-06 18:53:08 +0000

    Added new requirement initial work for python connection pool

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe','MySQL-python','simplejson'],
+      install_requires=['Flask>=0.7.2', 'MarkupSafe','MySQL-python','simplejson', 'DBUtils>=1.1'],
      )
