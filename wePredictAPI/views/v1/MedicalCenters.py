eaa1e33172ce0586c82fb5fe940a7bae8fe3bda9
robert <robrotheram@gmail.com>
2015-03-12 17:00:13 +0000

    Added list of CCG

--- wePredictAPI/views/v1/MedicalCenters.py
+++ wePredictAPI/views/v1/MedicalCenters.py
@@ -2,22 +2,23 @@ from flask import jsonify
 from flask import request
 
 from wePredictAPI.app import app
-from wePredictAPI.settings import *
-import MySQLdb
-
+from wePredictAPI.app  import databaseConnection
+from flask import Response
+import simplejson
 
 @app.route('/v1/getadress')
 def getAdress():
     limit = request.args.get('limit')
     if limit is None:
-        db = MySQLdb.connect(host=hostname, port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-        cursor.execute("SELECT * from ADRESS");
-        db.close()
-        return jsonify(data=cursor.fetchall())
+        return jsonify(data=databaseConnection.getResult("SELECT * from ADRESS LIMIT "))
     else:
-        db = MySQLdb.connect(host=hostname, port=3306, user=username, passwd=password, db=database)
-        cursor = db.cursor()
-        cursor.execute("SELECT * from ADRESS LIMIT " + limit);
-        db.close()
-        return jsonify(data=cursor.fetchall())
+        return jsonify(data=databaseConnection.getResult("SELECT * from ADRESS LIMIT " + limit))
+
+@app.route('/v1/getccg')
+def getCCG():
+    data = databaseConnection.getResult("SELECT CCG FROM CCG GROUP BY CCG")
+    js_data = []
+    for obj in data:
+        objjst = {"CCG": obj[0]}
+        js_data.append(objjst)
+    return Response(simplejson.dumps(js_data), mimetype='application/json')
