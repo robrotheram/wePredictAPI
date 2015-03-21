410ba817b45a2619f2cfe48f66f18e2f56ed4b68
robert <robrotheram@gmail.com>
2015-03-21 12:42:15 +0000

    Change connect pool

--- wePredictAPI/database/db.py
+++ wePredictAPI/database/db.py
@@ -1,29 +1,29 @@
 __author__ = 'robertfletcher'
 from flask import g
-from DBUtils.PooledDB import PooledDB
 from wePredictAPI.settings import *
 import MySQLdb
+import mysql.connector
+
 
 class DB(object):
 
     def __init__(self):
-        g.cnx_pool = PooledDB(creator = MySQLdb,
-                pool_name="name",
-                pool_size=10,
-                autocommit=True,
-                db = database,
-                host = hostname,
-                user = username,
-                passwd= password,
-                charset = "utf8")
+        g.cnx_pool = mysql.connector.pooling.MySQLConnectionPool(pool_name="name",
+                                                             pool_size=10,
+                                                             autocommit=True,
+                                                             user=username,
+                                                             password=password,
+                                                             host=hostname,
+                                                             database=database)
 
 
     def getConnection(self):
-        return g.cnx_pool.get_connection()
+        return self.pool.connection()
 
     def getResult(self,querry):
         conn = g.cnx_pool.get_connection()
         cursor = conn.cursor()
         cursor.execute(querry)
-        db.close()
+        #db.close()
         return cursor.fetchall()
+
