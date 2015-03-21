d0673b569dddd44c8ca2a496267d7f00bc9dd947
robert <robrotheram@gmail.com>
2015-03-21 16:58:54 +0000

    Change connect pool

--- wePredictAPI/app.py
+++ wePredictAPI/app.py
@@ -14,11 +14,11 @@ def before_request():
 
 import wePredictAPI.views.v1
 import wePredictAPI.views.v1.Asmtha
-#import wePredictAPI.views.v1.COPD
+import wePredictAPI.views.v1.COPD
 import wePredictAPI.views.v1.flu
 import wePredictAPI.views.v1.MedicalCenters
-#import wePredictAPI.views.v1.Pollution
-#import wePredictAPI.views.v1.smoking
+import wePredictAPI.views.v1.Pollution
+import wePredictAPI.views.v1.smoking
 
 if __name__ == '__main__':
     app.run()
