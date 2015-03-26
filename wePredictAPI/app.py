69f402cbb516d9e7fb9a10515e0d529c4653fac1
robertfletcher <robrotheram@gmail.com>
2015-03-26 11:48:17 +0000

    Inital api doumentation

--- wePredictAPI/app.py
+++ wePredictAPI/app.py
@@ -1,6 +1,11 @@
 from flask import Flask ,g
 from flask_cors import CORS
 from wePredictAPI.database.db import DB
+from flask import Flask, redirect
+from flask.ext.restful import reqparse, abort, Api, Resource, fields,\
+    marshal_with
+from flask_restful_swagger import swagger
+
 
 app = Flask(__name__)
 cors = CORS(app)
