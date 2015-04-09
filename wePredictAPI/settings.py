b3f50e3f0e5fbded48e590cdecfcfe3ae66f832c
robert <robrotheram@gmail.com>
2015-04-09 17:39:04 +0100

    reset db

--- wePredictAPI/settings.py
+++ wePredictAPI/settings.py
@@ -3,6 +3,10 @@ import os
 
 #global database varibles
 
+
+
+
+
 hostname=os.environ['OPENSHIFT_MYSQL_DB_HOST']
 username=os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
 password=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
