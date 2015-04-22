ad4827d082143d18c4f240e618e831276a9ee081
robert <robrotheram@gmail.com>
2015-04-22 16:48:29 +0100

    removed pointless files

--- wePredictAPI/settings.py
+++ wePredictAPI/settings.py
@@ -3,11 +3,9 @@ import os
 
 #global database varibles
 
-hostname="127.0.0.1"
-username="root"
-password="mallard"
 
-#hostname=os.environ['OPENSHIFT_MYSQL_DB_HOST']
-#username=os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
-#password=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
+
+hostname=os.environ['OPENSHIFT_MYSQL_DB_HOST']
+username=os.environ['OPENSHIFT_MYSQL_DB_USERNAME']
+password=os.environ['OPENSHIFT_MYSQL_DB_PASSWORD']
 database="wePredictV2"
